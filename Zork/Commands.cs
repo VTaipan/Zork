6f02caca9bda22fe26c5672af097e1327ab4b150
VTaipan <kwilson269@gmail.com>
2022-08-29 14:43:26 -0400

    Zork_1.2_init

---
+++ Zork/Commands.cs
@@ -0,0 +1,14 @@
+﻿namespace Zork
+
+{
+    public enum Commands
+    {
+        QUIT = 0,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
+}
--- Zork/Program.cs
+++ Zork/Program.cs
@@ -9,20 +9,53 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-            if(inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+        }
 
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
+        private static Commands ToCommand(string commandString)
+        {
+            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
         }
     }
+
 }
+//Commands command;
+//
+//if (commandString == "QUIT")
+//{
+//    Console.WriteLine("Thank you for playing.");
+//    command = Commands.QUIT;
+//}
+//
+//else if (commandString == "LOOK")
+//{
+//    Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+//    command = Commands.LOOK;
+//}
+//
+//else if (commandString == "NORTH")
+//{
+//    command = Commands.NORTH;
+//}
+//
+//else if (commandString == "SOUTH")
+//{
+//    command = Commands.SOUTH;
+//}
+//
+//else if (commandString == "EAST")
+//{
+//    command = Commands.EAST;
+//}
+//
+//else if (commandString == "WEST")
+//{
+//    command = Commands.WEST;
+//}
+//
+//else
+//{
+//    Console.WriteLine($"Unknown command: {inputString}");
+//    command = Commands.UNKNOWN;
+//}
