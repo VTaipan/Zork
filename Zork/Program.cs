e5d2dd604e45503502c63496724a2a34a7332412
VTaipan <kwilson269@gmail.com>
2022-08-31 14:56:22 -0400

    Zork_1.3_init

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,9 +8,49 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.Write("> ");
+                string inputString = Console.ReadLine();
+                Commands command = ToCommand(inputString.Trim().ToUpper());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        isRunning = false;
+                        outputString = "Thank you for playing!";
+                        break;
+                        
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                        outputString = "You moved NORTH";
+                        break;
+
+                    case Commands.SOUTH:
+                        outputString = "You moved SOUTH";
+                        break;
+
+                    case Commands.EAST:
+                        outputString = "You moved EAST";
+                        break;
+
+                    case Commands.WEST:
+                        outputString = "You moved WEST";
+                        break;
+          
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+                Console.WriteLine(outputString);
+            }
+            
         }
 
         private static Commands ToCommand(string commandString)
@@ -18,44 +58,4 @@ namespace Zork
             return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
         }
     }
-
 }
-//Commands command;
-//
-//if (commandString == "QUIT")
-//{
-//    Console.WriteLine("Thank you for playing.");
-//    command = Commands.QUIT;
-//}
-//
-//else if (commandString == "LOOK")
-//{
-//    Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-//    command = Commands.LOOK;
-//}
-//
-//else if (commandString == "NORTH")
-//{
-//    command = Commands.NORTH;
-//}
-//
-//else if (commandString == "SOUTH")
-//{
-//    command = Commands.SOUTH;
-//}
-//
-//else if (commandString == "EAST")
-//{
-//    command = Commands.EAST;
-//}
-//
-//else if (commandString == "WEST")
-//{
-//    command = Commands.WEST;
-//}
-//
-//else
-//{
-//    Console.WriteLine($"Unknown command: {inputString}");
-//    command = Commands.UNKNOWN;
-//}
