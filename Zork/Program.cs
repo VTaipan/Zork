16d006c8e68d90849642a9e96daa3938055a3bfc
VTaipan <kwilson269@gmail.com>
2022-09-26 14:49:40 -0400

    Zork_4.1_init

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,22 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Zork
 {
     class Program
     {
-        static void Main(string[] args)
+        private static Room CurrentRoom
         {
-
-            Console.WriteLine("Welcome to Zork!");
-
-            Game game = new Game();
-            game.Run();
-
-        
+            get
+            {
+                return _rooms[_location.Row, _location.Column];
+            }
         }
 
-        private void InitializeRoomDescriptions()
+        static void Main(string[] args)
+        {
+            InitializeRoomDescriptions();
+            Console.WriteLine("Welcome to Zork!");
+
+            Room previousRoom = CurrentRoom;
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.WriteLine(CurrentRoom);
+                if (previousRoom != CurrentRoom && CurrentRoom.HasBeenVisited == false)
+                {
+                    Console.WriteLine(CurrentRoom.Description);
+                    previousRoom = CurrentRoom;
+                    CurrentRoom.HasBeenVisited = true;
+                }
+                Console.Write("> ");
+
+
+                Commands command = Commands.UNKNOWN;
+                while (command != Commands.QUIT)
+                {
+                    command = ToCommand(Console.ReadLine().Trim());
+
+                    switch (command)
+                    {
+                        case Commands.QUIT:
+                            isRunning = false;
+                            Console.WriteLine("Thank you for playing!");
+                            break;
+
+                        case Commands.LOOK:
+                            Console.WriteLine(CurrentRoom.Description);
+                            break;
+
+                        case Commands.NORTH:
+                        case Commands.SOUTH:
+                        case Commands.EAST:
+                        case Commands.WEST:
+                            if (Move(command) == false)
+                            {
+                                Console.WriteLine("The way is shut!");
+                            }
+                            break;
+
+                        default:
+                            Console.WriteLine("Unknown command.");
+                            break;
+                    }
+
+                    Console.WriteLine(CurrentRoom);
+                }
+            }
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+        }
+
+        private static void InitializeRoomDescriptions(string Filename)
         {
             var roomMap = new Dictionary<string, Room>();
             foreach (Room room in _rooms)
@@ -24,18 +82,60 @@ namespace Zork
                 roomMap.Add(room.Name, room);
             }
 
-            roomMap["Rocky Trail"].Description = "You are facing a rock-stewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
+            const string fieldDelimiter = "##";
+            const int expectedFieldCount = 2;
+            var roomQuery = from line in Filename.ReadLines(roomsFilename)
+                            let fields = line.Split(fieldDelimiter)
+                            where fields.Length == expectedFieldCount
+                            select (Name: fields[(int)fields.Name],
+                                    Description: fields[(int)Fields.Description]);
 
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+            foreach (var (Name, Description) in roomQuery)
+            {
+                roomMap[name].Description = description;
+            }
 
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+        private static readonly Room[,] _rooms =
+            {
+                { new Room("Rocky Trail"), new Room ("South of House"), new Room ("Canyon View")},
+                { new Room("Forest"), new Room("West of House"), new Room("Behind House")},
+                { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing")}
+            };
+
+        private enum Fields
+        {
+            Name = 0,
+            Description = 1
         }
-    
+
+        private static (int Row, int Column) _location = (1, 1);
+        private static bool Move(Commands command)
+        {
+            bool didMove = false;
+
+            switch (command)
+            {
+                case Commands.NORTH when _location.Row < _rooms.GetLength(0) - 1:
+                    _location.Row++;
+                    didMove = true;
+                    break;
+
+                case Commands.SOUTH when _location.Row > 0:
+                    _location.Row--;
+                    didMove = true;
+                    break;
+
+                case Commands.EAST when _location.Column < _rooms.GetLength(1) - 1:
+                    _location.Column++;
+                    didMove = true;
+                    break;
+
+                case Commands.WEST when _location.Column > 0:
+                    _location.Column--;
+                    didMove = true;
+                    break;
+            }
+            return didMove;
+        }
     }
 }
